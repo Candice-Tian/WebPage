e7a23faea7e4da51eabb8b32baaf3f8161f4916c
Candice-Tian <tian779713952@163.com>
2021-08-22 21:19:52 +0800

    Commit test

---
+++ MVCPage/BLL/AddMethod.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCPage.BLL
+{
+    
+    public class AddMethod
+    {
+
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
